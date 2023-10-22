375b1e4672a50e90aef5667185590f5a31aa33ae
abdulllaha <abdullah.aw44@gmail.com>
2023-10-22 11:24:36 +0300

    build: install webpack and webpack plugins

---
+++ webpack.prod.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
+module.exports = {
+  entry: './src/client/index.js',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test:/.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  output: {
+    libraryTarget: 'var',
+    library: 'Client',
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/client/views/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({ filename: 'style.[contenthash].css' }),
+    new WorkboxPlugin.GenerateSW({
+        swDest: 'service-worker.js', // Output file name
+        clientsClaim: true,
+        skipWaiting: true,
+      }),  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+        new CssMinimizerPlugin(),
+        new TerserPlugin()
+    ],
+},
+};
